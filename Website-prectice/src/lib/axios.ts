985b55a88115df6d0829b2ebb158fe0c1fee39c8
Jenish Hariyani <hariyanijenish@gmail.com>
2026-05-28 10:35:56 +0530

    online backend connected

--- Website-prectice/src/lib/axios.ts
+++ Website-prectice/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: "https://learning-project-279l.onrender.com/",
 });
 
 api.interceptors.request.use((config) => {
