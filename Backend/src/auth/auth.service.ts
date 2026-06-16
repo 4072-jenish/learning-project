488c8123de354dabd0e152ba265f0a8e7b35117c
Jenish Hariyani <hariyanijenish@gmail.com>
2026-06-16 14:36:37 +0530

    backend error resolved

--- Backend/src/auth/auth.service.ts
+++ Backend/src/auth/auth.service.ts
@@ -103,52 +103,52 @@ export class AuthService {
     };
   }
 
-  private resend = new Resend(process.env.RESEND_API_KEY);
-  async sendOtpEmail(email: string) {
-    const otp = Math.floor(
-      100000 + Math.random() * 900000,
-    ).toString();
-    await this.resend.emails.send({
-      from: process.env.RESEND_FROM_EMAIL || 'Blog Platform <onboarding@resend.dev>',
-      to: email,
-      subject: 'Verify your email',
-      html: `<h1>${otp}</h1>`,
-    });
-    return {
-      success: true,
-      message: 'OTP sent successfully',
-    };
-  }
+  // private resend = new Resend(process.env.RESEND_API_KEY);
+  // async sendOtpEmail(email: string) {
+  //   const otp = Math.floor(
+  //     100000 + Math.random() * 900000,
+  //   ).toString();
+  //   await this.resend.emails.send({
+  //     from: process.env.RESEND_FROM_EMAIL || 'Blog Platform <onboarding@resend.dev>',
+  //     to: email,
+  //     subject: 'Verify your email',
+  //     html: `<h1>${otp}</h1>`,
+  //   });
+  //   return {
+  //     success: true,
+  //     message: 'OTP sent successfully',
+  //   };
+  // }
 
-  async verifyOtp(body: { email: string; otp: string }) {
-    const { email, otp } = body;
+  // async verifyOtp(body: { email: string; otp: string }) {
+  //   const { email, otp } = body;
 
-    const user = await this.userRepository.findOne({
-      where: { email },
-    });
-    if (!user) {
-      throw new BadRequestException('User not found');
-    }
-    if (!user.emailOtp) {
-      throw new BadRequestException( 'No OTP found. Please request a new OTP.' );
-    }
-    if (!user.emailOtpExpiresAt) {
-      throw new BadRequestException( 'OTP expiration not found.', );
-    }
-    if (new Date() > user.emailOtpExpiresAt) {
-      throw new BadRequestException( 'OTP has expired. Please request a new OTP.' );
-    }
-    if (user.emailOtp !== otp) {
-      throw new BadRequestException('Invalid OTP');
-    }
-    user.isEmailVerified = true;
-    user.emailOtp = null as any;
-    user.emailOtpExpiresAt = null as any;
-    await this.userRepository.save(user);
-    return {
-      success: true,
-      message: 'Email verified successfully',
-    };
-  }
+  //   const user = await this.userRepository.findOne({
+  //     where: { email },
+  //   });
+  //   if (!user) {
+  //     throw new BadRequestException('User not found');
+  //   }
+  //   if (!user.emailOtp) {
+  //     throw new BadRequestException( 'No OTP found. Please request a new OTP.' );
+  //   }
+  //   if (!user.emailOtpExpiresAt) {
+  //     throw new BadRequestException( 'OTP expiration not found.', );
+  //   }
+  //   if (new Date() > user.emailOtpExpiresAt) {
+  //     throw new BadRequestException( 'OTP has expired. Please request a new OTP.' );
+  //   }
+  //   if (user.emailOtp !== otp) {
+  //     throw new BadRequestException('Invalid OTP');
+  //   }
+  //   user.isEmailVerified = true;
+  //   user.emailOtp = null as any;
+  //   user.emailOtpExpiresAt = null as any;
+  //   await this.userRepository.save(user);
+  //   return {
+  //     success: true,
+  //     message: 'Email verified successfully',
+  //   };
+  // }
 
 }
